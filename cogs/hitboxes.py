31378e62bcc316ec5441820dc4111a65dbf1c156
JustSomeGuy2295 <jesasx775@gmail.com>
2025-10-27 15:56:04 +1000

    Added luigi command, requirements, and finished dk framedata
    - Luigi command fully working
    - Added discord, discord.ext, and redis to requirements.txt; configuring a virtual environment will work better now
    - Finished all the info for dk moves, no visuals added yet

--- cogs/hitboxes.py
+++ cogs/hitboxes.py
@@ -282,6 +282,23 @@ class Hitboxes(commands.Cog):
         ssf2_embed, view = ssf2_hitbox('Luffy', attack, interaction.user)
         await interaction.response.send_message(embed=ssf2_embed, view=view)
 
+    # Luigi
+    moves = Literal[
+        'Jab', 'Dash Attack',
+        'Down Tilt', 'Up Tilt', 'Forward Tilt',
+        'Neutral Air', 'Down Air', 'Up Air', 'Forward Air', 'Back Air',
+        'Down Smash', 'Up Smash', 'Forward Smash', 
+        'Up Special', 'Neutral Special',
+        'Down Special', 'Side Special',
+        'Grab', 'Forward Throw', 'Back Throw', 'Up Throw', 'Down Throw'
+    ]   
+    
+    @app_commands.command(name='luigi')
+    async def luigi(self, interaction: discord.Interaction, attack: moves):
+        """Luigi frame data and hitbox info"""
+        ssf2_embed, view = ssf2_hitbox('Luigi', attack, interaction.user)
+        await interaction.response.send_message(embed=ssf2_embed, view=view)
+
     # Mario
     moves = Literal[
         'Jab', 'Dash Attack',
