8f6cd2ccdd2927b35a29e7f17e924f7a6cdd5fa4
JustSomeGuy2295 <jesasx775@gmail.com>
2025-11-23 22:59:46 +1000

    Lloyd added
    Images and command added. Bot status updated.

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class MyBot(commands.Bot):
 
     def __init__(self, *, intents: discord.Intents):
         super().__init__(
-            activity=discord.CustomActivity(name='Luigi and Marth just added!'),
+            activity=discord.CustomActivity(name='Lloyd just added!'),
             command_prefix=commands.when_mentioned,
             intents=intents
         )
